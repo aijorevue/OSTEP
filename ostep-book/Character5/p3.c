3a2ca2cb018b9011a5fa93ce2a1db494c0cadf60
aijorevue <qinnovation013023@163.com>
2026-02-27 22:13:15 +0800

    the first commit

---
+++ ostep-book/Character5/p3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/wait.h>
+
+int main(int argc,char* argv[]){
+    printf("hello, world(parent pid:%d)\n",(int)getpid());
+    int rc=fork();
+    if(rc<0){
+        fprintf(stderr,"fork failed\n");
+        exit(1);
+    }
+    else if(rc==0){
+        printf("hello, I am child (pid:%d)\n",(int)getpid());
+        char *myargs[3];
+        myargs[0]=strdup("wc");
+        myargs[1]=strdup("p2.c");
+        myargs[2]=NULL;
+        execvp(myargs[0],myargs);
+    }
+    else{
+        int wc=wait(NULL);
+        printf("hello, I am parent of %d (pid:%d), wc=%d\n",rc,(int)getpid(),wc);
+    }
+    return 0;
+}
---
+++ ostep-book/Character5/p4.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<fcntl.h>
+
+int main(int argc,char *argv[]){
+    int rc=fork();
+    if(rc<0){
+        fprintf(stderr,"fork failed\n");
+        exit(1);
+    }
+    else if(rc==0){
+        close(STDOUT_FILENO);
+        open("p4.output",O_CREAT|O_WRONLY|O_TRUNC,S_IRWXU);
+        
+        char *myargs[3];
+        myargs[0]=strdup("ls");
+        myargs[1]=strdup("-l");
+        myargs[2]=NULL;
+        execvp(myargs[0],myargs);
+    }
+    else{
+        int wc=wait(NULL);
+    }
+    return 0;
+}
